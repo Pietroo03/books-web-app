f3b7d845f93692b58d2de5034eae276c270f4eb3
Pietroo03 <pietro.ponte.03@gmail.com>
2024-12-11 12:33:23 +0100

    import banner component

--- react-front/src/pages/BooksPage.jsx
+++ react-front/src/pages/BooksPage.jsx
@@ -1,4 +1,5 @@
 import BookCard from "../components/BookCard"
+import Banner from '../components/Banner'
 
 export default function BooksPage() {
 
@@ -67,22 +68,21 @@ export default function BooksPage() {
 
     return (
         <>
-            <div className="banner bg-primary text-white py-5">
-                <div className="container">
-                    <h1>Bool Books</h1>
-                    <h3>The nerdest book community</h3>
-                    <p className="lead">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea incidunt illo nihil dolor quas rerum! Ad, sed. Temporibus, provident sint nulla odio officia explicabo commodi consequuntur laborum, ab non quia!
-                    </p>
-                </div>
-            </div>
 
-            <section>
+            <Banner title={'Bool Books'} subtitle={'The nerdest book community'} leadtext={'Lorem ipsum dolor sit amet consectetur adipisicing.'} />
+
+            <section className="py-5">
                 <div className="container">
                     <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
-                        <div className="col">
-                            <BookCard />
-                        </div>
+                        {
+                            books.map(book => {
+
+                                <div className="col" key={book.id}>
+                                    <BookCard book={book} />
+                                </div>
+
+                            })
+                        }
                     </div>
                 </div>
             </section>
