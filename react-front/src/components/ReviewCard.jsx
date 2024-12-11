8544bd931013d2346a4c277a1217e64a201a6194
Pietroo03 <pietro.ponte.03@gmail.com>
2024-12-11 12:28:26 +0100

    add component for the review

---
+++ react-front/src/components/ReviewCard.jsx
@@ -0,0 +1,15 @@
+export default function ReviewCard({ review }) {
+
+    return (
+        <div className="review card mb-3">
+            <div className="card-body">
+                <p>{review.review}</p>
+                <span>By: {review.username}</span>
+
+                <div className="vote mt-3">
+                    <strong>Vote: {review.vote}</strong>
+                </div>
+            </div>
+        </div>
+    )
+}
