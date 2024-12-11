003bf9e530ae3777ccd3f8b2ffc5f1087051fda7
Pietroo03 <pietro.ponte.03@gmail.com>
2024-12-11 12:24:45 +0100

    add changes to footer component

--- react-front/src/components/Footer.jsx
+++ react-front/src/components/Footer.jsx
@@ -6,27 +6,36 @@ export default function Footer() {
                 <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3">
 
                     <div className="col">
-                        <h3>lorem</h3>
+                        <h3>Bool Books</h3>
                         <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex autem earum at sed aliquam repudiandae eveniet minima debitis sint repellendus.
                         </p>
+
+                        <div className="social d-flex">
+                            <i className="m-2 bi bi-facebook"></i>
+                            <i className="m-2 bi bi-twitter"></i>
+                            <i className="m-2 bi bi-instagram"></i>
+                        </div>
+
                     </div>
 
                     <div className="col">
                         <h4>Menu</h4>
                         <ul className="list-unstyled">
-                            <li><a href="#" className="text-white"></a></li>
-                            <li><a href="#" className="text-white"></a></li>
-                            <li><a href="#" className="text-white"></a></li>
-                            <li><a href="#" className="text-white"></a></li>
+                            <li><a href="#" className="text-white">Link 1</a></li>
+                            <li><a href="#" className="text-white">Link 2</a></li>
+                            <li><a href="#" className="text-white">Link 3</a></li>
+                            <li><a href="#" className="text-white">Link 4</a></li>
                         </ul>
+
                     </div>
+
 
                     <div className="col">
                         <h4>Legal Stuff</h4>
                         <ul className="list-unstyled">
-                            <li><a href="#" className="text-white"></a></li>
-                            <li><a href="#" className="text-white"></a></li>
+                            <li><a href="#" className="text-white">Link 1</a></li>
+                            <li><a href="#" className="text-white">Link 2</a></li>
                         </ul>
                     </div>
                 </div>
