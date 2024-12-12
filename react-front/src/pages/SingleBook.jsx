9bc55a0c8f7ab60faf143fb584a3d5b79572a613
Pietroo03 <pietro.ponte.03@gmail.com>
2024-12-12 10:57:05 +0100

    add fetch call from database

--- react-front/src/pages/SingleBook.jsx
+++ react-front/src/pages/SingleBook.jsx
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import Banner from '../components/Banner'
 import ReviewCard from '../components/ReviewCard'
 
@@ -6,42 +7,28 @@ export default function SingleBook() {
 
     const { id } = useParams()
 
-    const reviews = [
-        {
-            id: 1,
-            username: "Federico",
-            date: "2021-12-31T23:00:00.000Z",
-            review: "Un libro fantastico che mi ha fatto pensare.",
-            vote: 3
+    const base_book_api_server = `http://localhost:3000/api/books/${id}`
+    const [book, setBook] = useState(null)
 
-        },
-        {
-            id: 2,
-            username: "Manuel",
-            date: "2021-12-31T23:00:00.000Z",
-            review: "Un libro fantastico che mi ha fatto pensare.",
-            vote: 5
-
-        },
-        {
-            id: 3,
-            username: "Giorgia",
-            date: "2021-12-31T23:00:00.000Z",
-            review: "Un libro fantastico che mi ha fatto pensare.",
-            vote: 2
-
-        },
-    ]
+    useEffect(() => {
+        fetch(base_book_api_server)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setBook(data)
+            }).catch(err => console.error(err)
+            )
+    }, [])
 
     return (
 
         <>
 
-            <Banner title='Book Title' subtitle='By Author Name' leadtext='lorem ipsum dolor' />
+            <Banner title={book?.title} subtitle={`By ${book?.author}`} leadtext={book?.abstract} />
 
             <section className='reviews'>
                 <div className="container">
-                    {reviews.map((review) => <ReviewCard key={review.id} review={review} />)}
+                    {book && book.reviews.map((review) => <ReviewCard key={review.id} review={review} />)}
                 </div>
 
             </section>
